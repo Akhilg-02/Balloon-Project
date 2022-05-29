dfabdc292a714657effec57cd55c352a94398404
Akhil Gupta <93373025+Akhilg-02@users.noreply.github.com>
2022-05-30 01:21:23 +0530

    final commit

--- src/Components/Ballon.jsx
+++ src/Components/Ballon.jsx
@@ -13,7 +13,7 @@ export const Baloon = () => {
     var letters = "0123456789ABCDEF";
     var color = "#";
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 5)];
+      color += letters[Math.floor(Math.random() * 10)];
     }
     return color;
   }
